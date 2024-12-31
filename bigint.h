6bf230b734b6d4830d60dfdbc4e013ee9b1b3ecb
aaxlee <143385379+aaxlee@users.noreply.github.com>
2024-12-31 18:09:36 +0100

    Update bigint.h

--- bigint.h
+++ bigint.h
@@ -28,7 +28,7 @@ void bigint_exponent_print(bigint_t num);
 // bigint_printf() works like printf()
 // NOTE: the arguments must be bigint_t
 // %E => exponent form                  ex. 5.3E4
-// %N => decimal/standard form          ex. 5300
+// %N => standard notation              ex. 5300
 void bigint_printf(char *format, ...);
 /**********************/
 
